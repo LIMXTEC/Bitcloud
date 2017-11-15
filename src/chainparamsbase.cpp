029f806cc22ece069bfbef2b58adbb10ca21187e
Christian Knöpke <apfelbaum@email.de>
2017-11-15 07:33:09 +0100

    Change RPC DefaultPort

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -22,7 +22,7 @@ public:
     CBaseMainParams()
     {
         networkID = CBaseChainParams::MAIN;
-        nRPCPort = 51473;
+        nRPCPort = 8330;
     }
 };
 static CBaseMainParams mainParams;
--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -1031,7 +1031,7 @@ std::string HelpExampleRpc(string methodname, string args)
 {
     return "> curl --user myusername --data-binary '{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", "
            "\"method\": \"" +
-           methodname + "\", \"params\": [" + args + "] }' -H 'content-type: text/plain;' http://127.0.0.1:51473/\n";
+           methodname + "\", \"params\": [" + args + "] }' -H 'content-type: text/plain;' http://127.0.0.1:8330/\n";
 }
 
 const CRPCTable tableRPC;
