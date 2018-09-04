82ac71677646c3cf51354441f1a857fbb896f837
Chris <apfelbaum@email.de>
2018-09-04 23:11:29 +0200

    Bump Version

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70714;
+static const int PROTOCOL_VERSION = 70715;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -22,7 +22,7 @@ static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70714;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70714;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70715;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
