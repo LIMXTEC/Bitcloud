3287f756c21dd27d12a638cccbd4c656ae8acccf
Chris <apfelbaum@email.de>
2018-09-11 01:26:47 +0200

    Changes in Splash

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -34,7 +34,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle* networkStyle) 
     float fontFactor = 1.0;
 
     // define text to place
-    QString titleText = tr("Bitcloud BTX Core 12.2");
+    QString titleText = tr("Bitcloud BTDX Core 12.2");
     QString versionText = QString(tr("Version %1")).arg(QString::fromStdString(FormatFullVersion()));
     QString copyrightTextBtc = QChar(0xA9) + QString(" 2009-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Bitcoin Core developers"));
     QString copyrightTextDash = QChar(0xA9) + QString(" 2014-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Dash Core and PIVX developers"));
